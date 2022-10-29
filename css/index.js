e88b13cd40fef2c9f2b28f5809a10bba21228919
Claus3lido <cumplido26@gmail.com>
2022-10-29 00:59:15 -0700

    Update index.js

--- css/index.js
+++ css/index.js
@@ -5,20 +5,6 @@ function updateTime() {
   let sanDiegoTime = moment().tz("America/Los_Angeles");
   sanDiegoDatesElement.innerHTML = sanDiegoTime.format("MMMM Do YYYY");
   sanDiegoTimeElement.innerHTML = sanDiegoTime.format("h:mm:ss A");
-
-  let shanghaiElement = document.querySelector(".shanghai");
-  let shanghaiDatesElement = shanghaiElement.querySelector(".date");
-  let shanghaiTimeElement = shanghaiElement.querySelector(".time");
-  let shanghaiTime = moment().tz("Asia/Shanghai");
-  shanghaiDatesElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
-  shanghaiTimeElement.innerHTML = shanghaiTime.format("h:mm:ss A");
-
-  let dublinElement = document.querySelector(".dublin");
-  let dublinDatesElement = dublinElement.querySelector(".date");
-  let dublinTimeElement = dublinElement.querySelector(".time");
-  let dublinTime = moment().tz("Europe/dublin");
-  dublinDatesElement.innerHTML = dublinTime.format("MMMM Do YYYY");
-  dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss A");
 }
 
 updateTime();
