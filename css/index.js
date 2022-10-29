c1ffa4dcea78dab7258990a919b4a4f3becf3433
Claus3lido <cumplido26@gmail.com>
2022-10-29 02:09:09 -0700

    testing function

--- css/index.js
+++ css/index.js
@@ -26,13 +26,7 @@ function updateCity(event) {
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let timezoneElement = document.querySelector("#timezone");
-  timezoneElement.innerHTML = `<div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss A")}</div>
-    </div>
+  timezoneElement.innerHTML = `hello ${cityName}
      `;
 }
 updateTime();
